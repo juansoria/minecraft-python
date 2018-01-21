0774e5bc8c8245a7f2b1a3e7ac01c115da6da9ad
Juan Soria <juansoria@users.noreply.github.com>
2018-01-21 14:59:55 -0800

    More light w glowstone

--- Cavemaker.py
+++ Cavemaker.py
@@ -1,9 +1,10 @@
+# Good seed (Seed: 4307913003093348390) 
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 pos = mc.player.getPos()
 while True:
     pos = mc.player.getPos()
-    mc.setBlocks(pos.x - 2 , pos.y - 1, pos.z - 2, pos.x + 2, pos.y - 1, pos.z +2, 57)
-    mc.setBlocks(pos.x , pos.y, pos.z, pos.x, pos.y + 4, pos.z, 0)
-
-
+    mc.setBlocks(pos.x - 2 , pos.y - 1, pos.z - 2, pos.x + 2, pos.y - 1, pos.z +2, 1)
+    mc.setBlocks(pos.x , pos.y, pos.z, pos.x + 3, pos.y + 4, pos.z + 3, 0)
+    mc.setBlock(pos.x - 2 , pos.y , pos.z - 2, 89)
+    mc.setBlock(pos.x + 2 , pos.y , pos.z + 2, 89)
